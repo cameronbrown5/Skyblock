b2b3b6afd3f753351ee871b15e2b97b26c5ee971
Cam <thecamzone@gmail.com>
2024-07-11 15:00:44 -0600

    cHANGES

--- src/main/java/me/thecamzone/commands/DeveloperCommand.java
+++ src/main/java/me/thecamzone/commands/DeveloperCommand.java
@@ -20,9 +20,9 @@ public class DeveloperCommand extends BaseCommand {
     public DeveloperCommand(PaperCommandManager manager) {
         this.manager = manager;
 
-        manager.getCommandCompletions().registerCompletion("questCategories", c -> {
-            return ImmutableList.of("BUILDER");
-        });
+//        manager.getCommandCompletions().registerCompletion("questCategories", c -> {
+//            return ImmutableList.of("BUILDER");
+//        });
     }
 
     @Subcommand("openGUI")
@@ -30,18 +30,20 @@ public class DeveloperCommand extends BaseCommand {
     public void openGUI(CommandSender sender, String category, @Optional OnlinePlayer target, @Optional OnlinePlayer open) {
         QuestMenu questMenu = Skyblock.getInstance().getQuestMenu();
 
+        final String title = category + " Quests";
+
         if(sender instanceof Player player)
             if(target != null && open != null)
-                questMenu.getGui(target.getPlayer(), category.toLowerCase(), "Builder Quests").show(open.getPlayer());
+                questMenu.getGui(target.getPlayer(), category.toLowerCase(), title).show(open.getPlayer());
             else if(target != null)
-                questMenu.getGui(target.getPlayer(), category.toLowerCase(), "Builder Quests").show(player);
+                questMenu.getGui(target.getPlayer(), category.toLowerCase(), title).show(player);
             else
-                questMenu.getGui(player, category.toLowerCase(), "Builder Quests").show(player);
+                questMenu.getGui(player, category.toLowerCase(), title).show(player);
         else {
             if(target != null && open != null)
-                questMenu.getGui(target.getPlayer(), category.toLowerCase(), "Builder Quests").show(open.getPlayer());
+                questMenu.getGui(target.getPlayer(), category.toLowerCase(), title).show(open.getPlayer());
             else if(target != null)
-                questMenu.getGui(target.getPlayer(), category.toLowerCase(), "Builder Quests").show(target.getPlayer());
+                questMenu.getGui(target.getPlayer(), category.toLowerCase(), title).show(target.getPlayer());
             else {
                 sender.sendMessage(StringUtil.color("&cYou must specify a player to open the GUI on."));
             }
